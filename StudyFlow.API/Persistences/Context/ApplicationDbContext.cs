461ea3529e8bf7889f694d602b4b5f52dec0ea39
Mohamed El sayed <div.mohamedelsayed1@gmail.com>
2025-03-02 21:20:53 +0200

    update on delete cascade

--- StudyFlow.API/Persistences/Context/ApplicationDbContext.cs
+++ StudyFlow.API/Persistences/Context/ApplicationDbContext.cs
@@ -5,20 +5,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        // Apply all entity configurations from the current assembly
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-
-        // Identify all foreign keys that have Cascade delete behavior but are not ownership relationships
-        var cascadeFKs = builder.Model
-            .GetEntityTypes()
-            .SelectMany(t => t.GetForeignKeys())
-            .Where(fk => fk.DeleteBehavior == DeleteBehavior.Cascade && !fk.IsOwnership);
-
-        // Modify these foreign keys to use Restrict delete behavior instead of Cascade
-        foreach (var FK in cascadeFKs)
-            FK.DeleteBehavior = DeleteBehavior.Restrict;
-
-        // Call the base OnModelCreating method to finalize the model configuration
         base.OnModelCreating(builder);
     }
     public DbSet<Student> Students => Set<Student>();
